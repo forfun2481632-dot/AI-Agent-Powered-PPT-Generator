ab99111cd7b6f04ec448f194d1b804951e57f71b
forfun2481632-dot <forfun2481632@gmail.com>
2026-07-31 15:37:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ if all(ALL_API) and user_query:
 
   agent = create_agent(
     model = model,
-    tools = [search_latest_info
+    tools = [search_latest_info,
              generate_image]
   
   )
